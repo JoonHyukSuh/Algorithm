ba5aa0666573de7bccbe57a2c7cf705abbe07993
JoonHyukSuh <joon102938@gmail.com>
2022-04-17 19:10:08 +0900

    0417

--- Baekjoon/LV1/1157_단어 공부.py
+++ Baekjoon/LV1/1157_단어 공부.py
@@ -1,5 +1,11 @@
-example = input()
-example.upper()
+example = input().upper()
 
-unique_example = list(set(example))
-print(unique_example)
+keys = list(set(example))
+cnt = []
+for i in keys:
+    cnt.append(example.count(i))
+
+if cnt.count(max(cnt)) > 1:
+    print('?')
+else:
+    print(keys[cnt.index(max(cnt))])
