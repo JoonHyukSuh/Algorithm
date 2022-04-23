e572611ff2e58231d680c4c908ae5206e61d758d
JoonHyukSuh <joon102938@gmail.com>
2022-04-23 23:02:41 +0900

    0423

--- Baekjoon/LV1/2475_검증수.py
+++ Baekjoon/LV1/2475_검증수.py
@@ -1,7 +1,9 @@
 num = input().split()
 num_list = list(map(int,num))
+new_list = []
 for i in num_list:
-    i = i*i
-total = sum(num_list)
+    i = i**2
+    new_list.append(i)
+total = sum(new_list)
 answer = total % 10
 print(answer)
