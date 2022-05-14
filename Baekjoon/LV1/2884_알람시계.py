75d0437dba7db093f3d5471debbb52a745fada49
JoonHyukSuh <joon102938@gmail.com>
2022-05-14 14:34:34 +0900

    0514

--- Baekjoon/LV1/2884_알람시계.py
+++ Baekjoon/LV1/2884_알람시계.py
@@ -1,5 +1,12 @@
 h, m = map(int,input().split())
 if m >= 45:
     new_m = m - 45
+    new_h = h
 else:
-    new_m = m + 15 
+    new_m = m + 15
+    if h !=0:
+        new_h = h-1
+    else:
+        new_h = h+23
+print(new_h,new_m)
+
