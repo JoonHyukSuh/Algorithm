ee0fd688bae76e420e78b8d5db6a22079a1c4d45
JoonHyukSuh <joon102938@gmail.com>
2022-05-21 21:40:48 +0900

    0521

--- Baekjoon/LV1/8958_ox퀴즈.py
+++ Baekjoon/LV1/8958_ox퀴즈.py
@@ -1,4 +1,12 @@
 n = int(input())
 for i in range(n):
     case = str(input())
-    
+    score = 0
+    count = 0
+    for j in list(case):
+        if j == "O":
+            count += 1
+            score += count
+        elif j == "X":
+            count = 0
+    print(score)
