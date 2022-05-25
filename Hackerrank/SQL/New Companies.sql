13a3ed4d0cffae1f341c570bcef9f3025a2386a1
JoonHyukSuh <joon102938@gmail.com>
2022-05-25 23:16:17 +0900

    0525

--- Hackerrank/SQL/New Companies.sql
+++ Hackerrank/SQL/New Companies.sql
@@ -1,1 +1,7 @@
-select 
+select C.company_code, C.founder,
+        count(distinct LM.lead_manager_code),
+        count(distinct SM.senior_manager_code),
+        count(distinct M.manager_code),
+        count(distinct E.employee_code)
+from company C
+left join lead_manager LM on c
