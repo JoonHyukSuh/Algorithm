83a2f69ef92439af3bc2b456c106d2c11973af94
JoonHyukSuh <joon102938@gmail.com>
2022-06-18 21:26:40 +0900

    0618

--- Hackerrank/SQL/New Companies.sql
+++ Hackerrank/SQL/New Companies.sql
@@ -3,5 +3,10 @@ select C.company_code, C.founder,
         count(distinct SM.senior_manager_code),
         count(distinct M.manager_code),
         count(distinct E.employee_code)
-from company C
-left join lead_manager LM on c
+from company C, lead_manager LM, senior_manager SM, manager M, employee E 
+where C.company_code = LM.company_code and
+        LM.lead_manager_code = SM.lead_manager_code and
+        SM.senior_manager_code = M.senior_manager_code and
+        M.manager_code = E.manager_code 
+group by C.company_code, C.founder 
+order by C.company_code 
