9e1c8d566f325b0ae0109f3b324dcba07b3951be
JoonHyukSuh <joon102938@gmail.com>
2022-07-31 01:18:19 +0900

    0731

--- Baekjoon/LV2/10816_숫자카드2.py
+++ Baekjoon/LV2/10816_숫자카드2.py
@@ -1,21 +1,19 @@
 #10816 숫자카드2
 import sys
-input = sys.stdin.readline
+from collections import defaultdict
+#input = sys.stdin.readline
 n = int(input())
 nums = list(map(int,input().split()))
-
 m = int(input())
 cards = list(map(int,input().split()))
 
-cnt_list = []
+ans = defaultdict(int)
+
+for i in nums:
+  ans[i] += 1
 
 for i in cards:
-  cnt = 0
-  for j in range(len(nums)):
-    if i == nums[j]:
-      cnt += 1
-  cnt_list.append(cnt)
-
-
-for i in cnt_list:
-  print(i,end=" ")
+  if i in ans:
+    print(ans[i],end=' ')
+  else:
+    print(0,end=' ')
