73f5095bed51d261ee60f098b6e3de0fc30108cf
JoonHyukSuh <joon102938@gmail.com>
2022-09-08 10:56:21 +0900

    0908

---
+++ Baekjoon/LV3/2579_계단오르기.py
@@ -0,0 +1,16 @@
+#2579 계단 오르기
+import sys
+input = sys.stdin.readline
+n = int(input())
+scores = [0 for i in range(301)]
+for i in range(n):
+  scores[i] = int(input())
+dp = [0 for i in range(301)]
+
+dp[0] = scores[0]
+dp[1] = scores[0] + scores[1]
+dp[2] = max(scores[1]+scores[2], scores[0]+scores[2])
+
+for i in range(3,n):
+  dp[i] = max(dp[i-3]+scores[i-1]+scores[i],dp[i-2]+scores[i])
+print(dp[n-1])
